48bd3a0c56ead9b112f8f200f146d9e39c1a1433
looney4463 <130106763+looney4463@users.noreply.github.com>
2023-07-16 22:03:25 -0500

    Add files via upload

---
+++ factorial recursive and iterative.py
@@ -0,0 +1,34 @@
+"""
+Python Recursion Video Course
+Robin Andrews - https://compucademy.net/
+"""
+
+
+def factorial_recursive(n):
+    if n <= 1:
+        #base case
+        return 1
+    else:
+        #recursive case
+        return n * factorial_recursive(n-1)
+
+
+
+print(factorial_recursive(5))
+print(factorial_recursive(6))
+print(factorial_recursive(7))
+print(factorial_recursive(8))
+print(factorial_recursive(9))
+
+ 
+
+def factorial_iterative(n):
+        result = 1
+        for i in range(n):
+            result *= i + 1
+        return result
+print(factorial_iterative(5))
+print(factorial_iterative(6))
+print(factorial_iterative(7))
+print(factorial_iterative(8))
+print(factorial_iterative(9))
